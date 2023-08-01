ed2a5132afd7a8142988a82df6a78f7d5d082f16
Daniel Brandão <dcb@cin.ufpe.br>
2023-08-01 17:55:26 -0300

    Fix styles and change styled-components to mui styled

--- src/pages/Home/styles.tsx
+++ src/pages/Home/styles.tsx
@@ -1,12 +1,14 @@
-import styled from 'styled-components';
+import { styled } from '@mui/material';
 
-export const FilterContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  margin-top: 20px;
-`;
+export const FilterContainer = styled('div')({
+  display: 'flex',
+  flexDirection: 'row',
+  flexGrow: 1,
+  marginTop: '20px',
+  width: '100%',
+});
 
-export const FilterByAreaContainer = styled.div`
-  width: 20%;
-  margin-right: 20px;
-`;
+export const FilterByAreaContainer = styled('div')({
+  width: '20%',
+  marginRight: '20px',
+});
