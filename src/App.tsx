a01a48a2cbdd113722b1c59efd1567d936594395
Daniel Brandão <dcb@cin.ufpe.br>
2024-08-19 10:59:35 -0300

    Analytics initialize denied

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import { CssBaseline } from '@mui/material';
+import ReactGA from 'react-ga4';
 import RoutesApp from './routes';
 
 function App() {
+  ReactGA.gtag('consent', 'default', {
+    ad_storage: 'denied',
+    ad_user_data: 'denied',
+    ad_personalization: 'denied',
+    analytics_storage: 'denied',
+  });
+  if (process.env.REACT_APP_GOOGLE_ANALYTICS_ID) {
+    ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_ID);
+  }
+
   return (
     <>
       <CssBaseline />
